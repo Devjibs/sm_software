a70516a6c6f892795835b2ce52f57b541c2705b6
JiboGithub <ajidejibola@yahoo.com>
2021-07-24 19:36:42 +0100

    updated design

--- config/app.php
+++ config/app.php
@@ -41,6 +41,13 @@ return [
 
     'debug' => (bool) env('APP_DEBUG', false),
 
+
+    'debug_blacklist' => [
+        '_COOKIE' => array_keys($_COOKIE),
+        '_SERVER' => array_keys($_SERVER),
+        '_ENV' => array_keys($_ENV)
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Application URL
